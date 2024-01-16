49d37a94bcf678323a6d1ecf77d869f85a7648d9
ROSEWT <rody.vilchez00@gmail.com>
2024-01-16 18:33:40 -0500

    Juego base implementado

--- src/api_gpt.py
+++ src/api_gpt.py
@@ -53,11 +53,55 @@ def show_get_respuesta_GPT(chat_history, prompt, temp, OPENAI_API_KEY):
     print(full_r_content)
     print("\033[H\033[J", end="")
 
-
-
-  print(chat_history)
+  #print(chat_history)
   print(full_r_content)
   # aniadimos la respuesta al historial
+  chat_history.append({'role': 'assistant', 'content': full_r_content})
+
+  return full_r_content , chat_history
+
+
+def check_esc(text):
+   esc = 'Alternativa Correcta:'
+   return text.endswith(esc)
+
+# buged
+def show_get_r_GPT(chat_history, prompt, temp, OPENAI_API_KEY): 
+  # aniadimos el prompt al historial
+  chat_history.append({'role': 'user', 'content': prompt})
+  # nos conectamos con gpt3.5 y le pedimos la respuesta en modo stream
+  client = OpenAI(api_key=OPENAI_API_KEY)
+  chat_completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    temperature=temp,
+    max_tokens=255,
+    messages=chat_history
+  ,
+  stream=True
+)
+  mensaje = []
+  # print while writing
+  
+  save_alternativa = False
+
+  for chunk in chat_completion:
+    c_msg = chunk.choices[0].delta.content
+    mensaje.append(c_msg)
+    if save_alternativa:
+      alternativa = ''.join(str(i) for i in mensaje if i is not None) 
+      print(alternativa)
+    else:
+      full_r_content = ''.join(str(i) for i in mensaje if i is not None)
+      print(full_r_content)
+      print("\033[H\033[J", end="")
+   
+    if check_esc(full_r_content):
+      save_alternativa = True
+
+  #print(chat_history)
+  print(full_r_content)
+  # aniadimos la respuesta al historial
+  full_r_content += alternativa
   chat_history.append({'role': 'assistant', 'content': full_r_content})
 
   return full_r_content , chat_history
--- src/main.py
+++ src/main.py
@@ -3,43 +3,56 @@ import api_gpt
 
 
 def config_prompt():
-    prompt = '''Haremos una dinámica, realiza un juego para aprender a elegir el equipo de protección
-        personal adecuado para la actividad de riesgo. El jugador es un trabajador de una empresa de 
-        distribucion electrica de 45 años. Para cada actividad de riesgo brinda tres alternativas de 
-        conjugación,donde al menos una es correcta, brindame las(s) alternativas correctas. De forma 
-        aleatoria selecciona un nivel de dificultad (0,1,2) . Realiza una pregunta a la vez. Continua 
-        hasta que te diga "stop". Empieza por la primera pregunta.
-Dificultad: <Dificultad>
-Pregunta:<Pregunta>
-Alternativa:<Alternativas>
-Alternativa correcta: <Alternativa correcta>
-'''
-    p_rol = 'Toma el rol de un ingeniero de seguridad industrial. '
-    return prompt, p_rol
+    prompt = '''Como ingeniero de seguridad industrial, participarás en un juego interactivo que te permitirá 
+    aprender a seleccionar el equipo de protección personal adecuado para diferentes actividades de riesgo.
+    Tu personaje es un trabajador de 45 años de una empresa de distribución eléctrica.En cada turno, se te 
+    presentará una actividad de riesgo y se te proporcionarán tres alternativas de equipos de protección personal. 
+    Deberás seleccionar la alternativa correcta basándote en tu conocimiento y experiencia. Además, el juego variará 
+    en dificultad. De forma aleatoria, se seleccionará un nivel de dificultad entre capacitado, experto y veterano. 
+    Esto afectará a la complejidad de las actividades de riesgo y las opciones de equipos de protección personal.
+    Después de cada elección, recibirás retroalimentación y una explicación sobre por qué la alternativa seleccionada es correcta o incorrecta. Además, se te recordará tu puntaje acumulado.
+    La estructura de cada turno será la siguiente:
 
+    - Puntaje: <Puntaje>
+    - Dificultad: <Dificultad>
+    - Pregunta: <Pregunta o caso>
+    - Alternativa: <Alternativas>
+
+    Tras seleccionar una alternativa inmediatamente empieza otro turno.¡Empecemos con la primera pregunta!
+'''
+
+    return prompt
+
+
+def get_alternativa():
+    a = '0'
+    while a != '1' and a != '2' and a != '3' and a != 'exit':
+        a = input(' ')
+    return a
 
 #####################################################################################################
 def main():
-    API_OPEN_AI_KEY = config('API_OPEN_AI_KEY')
+    OPEN_AI_KEY = config('OPEN_AI_KEY')
     chat_history = []
     temperature = .2
+    prompt = config_prompt()
+    preguntas = 2
 
-    while True:
-        prompt = input('Digita Prompt: ')
+    while preguntas > 0:
         if prompt == 'exit':
             break
         else:
-            response , chat_history = api_gpt.show_get_respuesta_GPT(chat_history, prompt, temperature, API_OPEN_AI_KEY)
-
-
-    #prompt, p_rol = config_prompt()
-            
-    ## inicializar el juego
-    #respuesta = api_gpt.get_respuesta_GPT(prompt, p_rol, .3, API_OPEN_AI_KEY)
-    #print(respuesta)
-    # guardar respuestas
-    # salir del juego
-    # comunicar resultados
-
+            response , chat_history = api_gpt.show_get_respuesta_GPT(chat_history, prompt, temperature, OPEN_AI_KEY)  
+        preguntas -= 1
+        prompt = get_alternativa()
+        if preguntas == 0:
+            prompt = '''Motivame a seguir aprendiendo y recuerdame mi puntaje:
+                        La estructura será la siguiente:
+                        Tu puntaje: <Puntaje>
+                        <Mensaje motivador>'''
+            temperature = .7 # para que se ponga creativo
+            response , chat_history = api_gpt.show_get_respuesta_GPT(chat_history, prompt, temperature, OPEN_AI_KEY) 
+        
+    
 if __name__ == "__main__":
     main()
