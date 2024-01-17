b8ce7244294ee4554af18e0af38a296ce721d2c4
ROSEWT <rody.vilchez00@gmail.com>
2024-01-16 19:04:34 -0500

    Personalizacion usando un excel como bd e

---
+++ src/c_empleado.py
@@ -0,0 +1,7 @@
+class Empleado:
+    def __init__(self, name, lname, rol, age):
+       self.name = name
+       self.lname = lname
+       self.rol = rol
+       self.age = age
+
--- src/main.py
+++ src/main.py
@@ -1,11 +1,27 @@
 from decouple import config
 import api_gpt
+import c_empleado
+import pandas as pd
+import random
 
 
-def config_prompt():
-    prompt = '''Como ingeniero de seguridad industrial, participarás en un juego interactivo que te permitirá 
+def get_empleado(path):
+    empleados = []
+    for _ ,row in pd.read_excel(path).iterrows():
+        empleado = c_empleado.Empleado( row['Nombre'], row['Apellidos']
+                                       , row['Cargo'], row['Edad']
+                                       )
+        empleados.append(empleado)
+
+    # incio de sesion
+    user = random.choice(empleados)
+    return user
+
+
+def config_prompt(user):
+    prompt = f'''Como {user.rol}, participarás en un juego interactivo que te permitirá 
     aprender a seleccionar el equipo de protección personal adecuado para diferentes actividades de riesgo.
-    Tu personaje es un trabajador de 45 años de una empresa de distribución eléctrica.En cada turno, se te 
+    Tu personaje es un trabajador de {user.age} años de una empresa de distribución eléctrica.En cada turno, se te 
     presentará una actividad de riesgo y se te proporcionarán tres alternativas de equipos de protección personal. 
     Deberás seleccionar la alternativa correcta basándote en tu conocimiento y experiencia. Además, el juego variará 
     en dificultad. De forma aleatoria, se seleccionará un nivel de dificultad entre capacitado, experto y veterano. 
@@ -32,11 +48,16 @@ def get_alternativa():
 
 #####################################################################################################
 def main():
+    path = './empleados.xlsx'
+    user = get_empleado(path)
+    prompt = config_prompt(user)
+
     OPEN_AI_KEY = config('OPEN_AI_KEY')
     chat_history = []
     temperature = .2
-    prompt = config_prompt()
     preguntas = 2
+    
+    print(f'Hola {user.rol} {user.name} {user.lname}')
 
     while preguntas > 0:
         if prompt == 'exit':
